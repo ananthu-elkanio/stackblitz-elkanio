ec39758db821d4e2e486c07d2e29cf44b928de92
Avas <34262142+ananthu-va@users.noreply.github.com>
2023-06-13 15:43:35 +0530

    ui

--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -9,10 +9,14 @@ export class ServiceService {
 
   private apiUrl = 'https://reqres.in';
 
+  // post - /api/users
+  // get single user - /api/users/2
+
   constructor(private http: HttpClient) { }
 
   getUsersList(): Observable<any> {
     const url = `${this.apiUrl}/api/users`;
     return this.http.get(url);
   }
+  
 }
